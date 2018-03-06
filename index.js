fc80151330f8112ceea70ef8186247aa7501b883
Nicolas Marini <nmarini44@gmail.com>
2018-03-06 23:27:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,13 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
 }
